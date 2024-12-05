b9b15cf163590107dbb8182f06263262ecb5891c
kirillench <kirillbogacev651@gmail.com>
2024-12-05 22:45:35 +0300

    new

--- main/home/models.py
+++ main/home/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class OrganizationalMember(models.Model):
-    name = models.CharField(max_length=255)
-    surname = models.CharField(max_length=255)
-    position = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20)
+    name = models.CharField(max_length=255, verbose_name="Имя")
+    surname = models.CharField(max_length=255, verbose_name="Фамилия")
+    position = models.CharField(max_length=255, verbose_name="Должность")
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone_number = models.CharField(max_length=20, verbose_name="Номер телефона")
     supervisor = models.ForeignKey(
-        'self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subordinates'
+        'self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subordinates', verbose_name="Ссылка на руководителя"
     )  # Ссылка на руководителя
 
     def __str__(self):
--- main/main/settings.py
+++ main/main/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'challenge',
+        'NAME': 'challenges',
         'USER': 'postgres',
         'PASSWORD': '1',
         'HOST': 'localhost',
